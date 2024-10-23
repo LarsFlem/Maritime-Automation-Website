7b32fbef3fb50421388ed07bcc5895d0fb00e99b
LarsFlem <43146722+LarsFlem@users.noreply.github.com>
2024-10-23 13:57:45 +0200

    Update PDF.tsx

--- src/components/PDF.tsx
+++ src/components/PDF.tsx
@@ -1,18 +1,21 @@
 import React, { useEffect, useRef } from "react";
 import "./PDF.css";
-
-// Import the PDF file
 import CV_PDF from "./_PDF/Lars_Flem_CV_english.pdf";
 
-// Define the PdfViewer component
 const PdfViewer: React.FC = () => {
   const pdfRef = useRef<HTMLObjectElement>(null);
 
   useEffect(() => {
     if (pdfRef.current) {
-      // Calculate the height dynamically based on the window height
       const windowHeight = window.innerHeight;
-      pdfRef.current.style.height = `${windowHeight - 150}px`; // Subtract header/footer or any margin as needed
+
+      if (window.innerWidth > 768) {
+        // Set height dynamically for larger screens
+        pdfRef.current.style.height = `${windowHeight - 150}px`; // Adjust as needed
+      } else {
+        // On mobile, allow scrolling and set auto height
+        pdfRef.current.style.height = "auto";
+      }
     }
   }, []);
 
