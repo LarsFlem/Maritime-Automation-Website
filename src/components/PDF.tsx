7cd7841c8fb4eb47c8a0ceb10480023a2c05e842
LarsFlem <43146722+LarsFlem@users.noreply.github.com>
2024-10-23 13:44:32 +0200

    Update PDF.tsx

--- src/components/PDF.tsx
+++ src/components/PDF.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./PDF.css";
 
 // Import the PDF file
@@ -6,10 +6,20 @@ import CV_PDF from "./_PDF/Lars_Flem_CV_english.pdf";
 
 // Define the PdfViewer component
 const PdfViewer: React.FC = () => {
+  const pdfRef = useRef<HTMLObjectElement>(null);
+
+  useEffect(() => {
+    if (pdfRef.current) {
+      // Calculate the height dynamically based on the window height
+      const windowHeight = window.innerHeight;
+      pdfRef.current.style.height = `${windowHeight - 150}px`; // Subtract header/footer or any margin as needed
+    }
+  }, []);
+
   return (
     <div className="pdf-viewer-container">
       <h2>CV</h2>
-      <object data={CV_PDF} type="application/pdf" width="100%" height="750px">
+      <object ref={pdfRef} data={CV_PDF} type="application/pdf" width="100%">
         <p>
           PDF cannot be displayed. <a href={CV_PDF}>Download the PDF</a>.
         </p>
