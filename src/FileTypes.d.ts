e9df4bca36842bd5fb0fa7ab8e9c94bc17e3bc98
Lars Flem <Lars.Flem@gmail.com>
2025-08-01 15:07:13 +0200

    Addedmissing link

--- src/FileTypes.d.ts
+++ src/FileTypes.d.ts
@@ -12,3 +12,9 @@ declare module "*.pdf" {
   const src: string;
   export default src;
 }
+
+declare module 'react-pdf' {
+  import * as React from 'react';
+  export const Document: React.FC<any>;
+  export const Page: React.FC<any>;
+}
